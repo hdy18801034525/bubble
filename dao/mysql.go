2c420631646791c18c1b49d40d24e433b1949436
hdy <1738572247@qq.com>
2022-01-17 23:06:56 +0800

    第一次修改提交

---
+++ dao/mysql.go
@@ -0,0 +1,25 @@
+package dao
+
+import (
+	// "bubble/setting"
+	// "fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func InitMySQL() (err error) {
+	dsn := "root:l6331436@tcp(127.0.0.1:3306)/%sbubble?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	return DB.DB().Ping()
+}
+
+func Close() {
+	DB.Close()
+}
